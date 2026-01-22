6c02b5a220f9946b77698befbfe28d635fcfcef5
roygicheru <gicheru.k.roy@gmail.com>
2026-01-22 09:24:47 +0300

    Added relevant comments

--- src/main/java/com/github/roygicheru/Exercises/JavaSE007/BankAccount.java
+++ src/main/java/com/github/roygicheru/Exercises/JavaSE007/BankAccount.java
@@ -1,16 +1,25 @@
 package com.github.roygicheru.Exercises.JavaSE007;
 
+/**
+ * A class representing a bank account with basic operations.
+ * @author Roy Gicheru
+ */
+
 public class BankAccount {
 
+    // Instance variables for account number, account holder, and balance
     public String accountNumber;
     public String accountHolder;
     public Double balance;
 
+    // Constructor to initialize a bank account
     public BankAccount(String accountNumber, String accountHolder, Double balance) {
         this.accountNumber = accountNumber;
         this.accountHolder = accountHolder;
         this.balance = balance;
     }
+
+    // Alternative constructors can be added as needed
 
 //    public BankAccount_RoyGicheru(String accountNumber, String accountHolder) {
 //        this.accountNumber = accountNumber;
@@ -30,6 +39,7 @@ public class BankAccount {
 //        this.balance = 0.0;
 //    }
 
+    // Method to deposit money into the account
     public void deposit(double amount) {
         if (amount > 0) {
             this.balance += amount;
@@ -38,6 +48,7 @@ public class BankAccount {
         }
     }
 
+    // Method to withdraw money from the account
     public void withdraw(double amount) {
         if (amount > 0) {
             if (this.balance >= amount) {
@@ -50,6 +61,7 @@ public class BankAccount {
         }
     }
 
+    // Method to display the current balance
     public void displayBalance() {
         System.out.println("The balance of user "+ this.accountHolder + " of account number " + this.accountNumber +" is " + this.balance);
     }
--- src/main/java/com/github/roygicheru/Exercises/JavaSE007/BankAccountAssignment_RoyGicheru.java
+++ src/main/java/com/github/roygicheru/Exercises/JavaSE007/BankAccountAssignment_RoyGicheru.java
@@ -1,10 +1,11 @@
 package com.github.roygicheru.Exercises.JavaSE007;
 
 public class BankAccountAssignment_RoyGicheru {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Test the BankAccount class
+        // Create a bank account with initial user and account details
         BankAccount bankAccount = new BankAccount("101000", "Roy Gicheru", 30000.0);
-        bankAccount.deposit(20000);
-        bankAccount.withdraw(10000);
-        bankAccount.displayBalance();
+        bankAccount.deposit(20000); // Deposit money
+        bankAccount.withdraw(10000); // Withdraw money
+        bankAccount.displayBalance(); // Display the current balance
     }
 }
