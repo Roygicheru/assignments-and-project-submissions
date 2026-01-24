f3f035f088a7dfcf7378cac9d76687fdb850f62c
roygicheru <gicheru.k.roy@gmail.com>
2026-01-24 22:42:03 +0300

    Added encapsulation and flexibility to bank code

--- src/main/java/com/github/roygicheru/Exercises/JavaSE007/BankAccount.java
+++ src/main/java/com/github/roygicheru/Exercises/JavaSE007/BankAccount.java
@@ -8,51 +8,59 @@ package com.github.roygicheru.Exercises.JavaSE007;
 public class BankAccount {
 
     // Instance variables for account number, account holder, and balance
-    public String accountNumber;
-    public String accountHolder;
-    public Double balance;
+    private final String accountNumber;
+    private final String accountHolder;
+    private double balance;
 
     // Constructor to initialize a bank account
-    public BankAccount(String accountNumber, String accountHolder, Double balance) {
+    public BankAccount(String accountNumber, String accountHolder, double initialBalance) {
         this.accountNumber = accountNumber;
         this.accountHolder = accountHolder;
-        this.balance = balance;
+        if (initialBalance >= 0) {
+            this.balance = initialBalance;
+            System.out.println("\nAccount created for " + accountHolder + " of account number " + accountNumber + " with initial balance: " + initialBalance);
+        } else {
+            this.balance = 0.0;
+            System.out.println("Initial balance cannot be negative, setting it to 0.0");
+        }
     }
 
-    // Alternative constructors can be added as needed
+    // Alternative constructors for more flexibility
 
-//    public BankAccount_RoyGicheru(String accountNumber, String accountHolder) {
-//        this.accountNumber = accountNumber;
-//        this.accountHolder = accountHolder;
-//        this.balance = 0.0;
-//    }
-//
-//    public BankAccount_RoyGicheru(String accountNumber) {
-//        this.accountNumber = accountNumber;
-//        this.accountHolder = null;
-//        this.balance = 0.0;
-//    }
-//
-//    public BankAccount_RoyGicheru() {
-//        this.accountNumber = null;
-//        this.accountHolder = null;
-//        this.balance = 0.0;
-//    }
+   public BankAccount(String accountNumber, String accountHolder) {
+       this.accountNumber = accountNumber;
+       this.accountHolder = accountHolder;
+       this.balance = 0.0;
+   }
+
+   public BankAccount(String accountNumber) {
+       this.accountNumber = accountNumber;
+       this.accountHolder = null;
+       this.balance = 0.0;
+   }
+
+   public BankAccount() {
+       this.accountNumber = null;
+       this.accountHolder = null;
+       this.balance = 0.0;
+   }
 
     // Method to deposit money into the account
-    public void deposit(double amount) {
-        if (amount > 0) {
-            this.balance += amount;
+    public void deposit(double depositAmount) {
+        if (depositAmount > 0) {
+            this.balance += depositAmount;
+            System.out.println("\nDeposited: " + depositAmount);
         } else {
             System.out.println("Invalid deposit amount.");
         }
     }
 
     // Method to withdraw money from the account
-    public void withdraw(double amount) {
-        if (amount > 0) {
-            if (this.balance >= amount) {
-                this.balance -= amount;
+    public void withdraw(double withdrawAmount) {
+        if (withdrawAmount > 0) {
+            if (this.balance >= withdrawAmount) {
+                this.balance -= withdrawAmount;
+                System.out.println("Withdrew: " + withdrawAmount);
             } else {
                 System.out.println("Insufficient funds!");
             }
@@ -63,6 +71,6 @@ public class BankAccount {
 
     // Method to display the current balance
     public void displayBalance() {
-        System.out.println("The balance of user "+ this.accountHolder + " of account number " + this.accountNumber +" is " + this.balance);
+        System.out.println("\nThe balance of user "+ this.accountHolder + " of account number " + this.accountNumber +" is " + this.balance + "\n");
     }
 }
