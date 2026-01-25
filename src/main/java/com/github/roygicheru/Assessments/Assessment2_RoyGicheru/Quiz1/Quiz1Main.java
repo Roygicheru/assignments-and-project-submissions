01fd40f3553c7b44440414b87cd90acaf99426b0
roygicheru <gicheru.k.roy@gmail.com>
2026-01-25 12:22:00 +0300

    Added relevant comments

--- src/main/java/com/github/roygicheru/Assessments/Assessment2_RoyGicheru/Quiz1/Quiz1Main.java
+++ src/main/java/com/github/roygicheru/Assessments/Assessment2_RoyGicheru/Quiz1/Quiz1Main.java
@@ -1,5 +1,9 @@
 package com.github.roygicheru.Assessments.Assessment2_RoyGicheru.Quiz1;
-
+/* This is a project meant to demonstrate encapsulation
+    * using a Book class with private fields and public methods
+    * to borrow and return a book.
+    *@author Roy Gicheru
+ */
 public class Quiz1Main {
     public static void main(String[] args){
         Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "1234567890123", 10.99, true);
