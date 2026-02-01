1d75f9160479484562069809379deefae74c4019
roygicheru <gicheru.k.roy@gmail.com>
2026-02-01 10:12:25 +0300

    Added temporary test code to ecommerce test

--- src/main/java/com/github/roygicheru/Assessments/Assessment3_RoyGicheru/Problem3/ECommerceTest.java
+++ src/main/java/com/github/roygicheru/Assessments/Assessment3_RoyGicheru/Problem3/ECommerceTest.java
@@ -1,5 +1,7 @@
 package com.github.roygicheru.Assessments.Assessment3_RoyGicheru.Problem3;
 
 public class ECommerceTest {
-    
+    public static void main(String[] args) {
+        System.out.println("Welcome to the E-Commerce Platform Test!");
+    }
 }
